d18d42a955786bd81e5d4883339f1217ddce61ed
Marco.A.D.S <marcosofth@gmail.com>
2024-09-11 23:59:17 -0300

    Estrutura Condicionais

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿string? letra = Console.ReadLine();
+
+    while((string.IsNullOrEmpty(letra)))
+    {
+        if ((string.IsNullOrEmpty(letra)))
+        {
+            Console.WriteLine("Insira uma letra: ");
+            letra = Console.ReadLine();
+        }
+    }
+
+    switch (letra.ToLower())
+    {
+        case "a":
+        case "e":
+        case "i":
+        case "o":
+        case "u":
+
+        Console.WriteLine("A letra inserida, é uma vogal");
+        break;
+
+        case null:
+        Console.WriteLine("Insira uma letra: ");
+        break;
+
+        default : Console.WriteLine("A letra inserida, não é uma vogal");
+        break;
+    }
